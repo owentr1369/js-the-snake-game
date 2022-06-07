da51e19728e1381da9a86cbc63062c5be15d2eaa
owentr1369 <tamvaa1306@gmail.com>
2022-06-07 16:52:28 +0700

    equalPositions and addSegments

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 import { getInputDirection } from "./input.js";
-
+let newSegments = 0;
 export const SNAKE_SPEED = 5;
 const snakeBody = [
   { x: 11, y: 11 },
@@ -7,6 +7,7 @@ const snakeBody = [
 ];
 
 export function update() {
+  addSegments();
   const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
@@ -25,4 +26,25 @@ export function draw(gameBoard) {
 
     gameBoard.appendChild(snakeElement);
   });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some((segment) => {
+    return equalPositions(segment, position);
+  });
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...(snakeBody[snakeBody.length] - 1) });
+  }
+  newSegments = 0;
 }

1303ba23da1b8f81be93062982b4919060f40c51
owentr1369 <tamvaa1306@gmail.com>
2022-06-07 17:26:15 +0700

    completed

--- grid.js
+++ grid.js
@@ -1,8 +1,17 @@
 const GRID_SIZE = 21;
 
-export function randomGridPostition() {
+export function randomGridPosition() {
   return {
-    x: Math.floor(Math.random() * GRID_SIZE + 1),
-    y: Math.floor(Math.random() * GRID_SIZE),
+    x: Math.floor(Math.random() * GRID_SIZE) + 1,
+    y: Math.floor(Math.random() * GRID_SIZE) + 1,
   };
+}
+
+export function outsideGrid(position) {
+  return (
+    position.x < 1 ||
+    position.x > GRID_SIZE ||
+    position.y < 1 ||
+    position.y > GRID_SIZE
+  );
 }

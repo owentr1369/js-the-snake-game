50c7406f9a67c474ab14fba55ccc4c8ed4577701
owentr1369 <tamvaa1306@gmail.com>
2022-06-07 16:06:59 +0700

    drawSnake functionality

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-  console.log("Render");
   lastRenderTime = currentTime;
 
   update();
@@ -14,3 +19,10 @@ function main(currentTime) {
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+function draw() {
+  drawSnake(gameBoard);
+}
